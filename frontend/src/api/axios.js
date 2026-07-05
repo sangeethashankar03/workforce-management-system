5adb963ae0749ce948885bf1b2daa7d45d21aedd
sangeethashankar03 <20097745@mydbs.ie>
2026-07-05 20:28:58 +0100

    Updated frontend url to use the deployed backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://workforce-management-system-mgkzkd7ey.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
